4bdd2a9f993dd46628ab7a27df5c63690f050c75
karn322 <tanapatkarn58@gmail.com>
2022-02-26 16:31:37 +0700

    re name user 2,3

--- src/FriendList.js
+++ src/FriendList.js
@@ -8,15 +8,14 @@ import FriendListBox from './components/FriendListBox';
 function FriendList(){
   const [friends, setFriends] = useState([
     {user: "user1", displayName: "ไอ้ปื้ด"},
-    {user: "user2", displayName: "นางสาวส้ม"},
-    {user: "user3", displayName: "พลทหารแดง"},
+    {user: "user2", displayName: "นางสาวสุดา"},
+    {user: "user3", displayName: "พลทหารหนึ่ง"},
     {user: "user4", displayName: "ศาตราจารย์นายกรัฐมนตรีดอกเตอร์เขียว"},
   ]);
 
   return(
     <App>
       <div className="friend-list">
-        <Userlist/>
         <FriendListBox data={friends}/>
       </div>
     </App>
