99ac64d02656536e14e54c4c2ac4843a693704c9
karn322 <tanapatkarn58@gmail.com>
2022-03-12 16:56:27 +0700

    add teststate js and css

---
+++ src/TestState.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import './TestState.css'
+
+function TestState(){
+    const [size , setSize] = useState("small")
+
+    function changeSize(){
+        if(size === "small"){
+            setSize("medium")
+        }
+        if(size === "medium"){
+            setSize("large")
+        }
+        if(size === "large"){
+            setSize("small")
+        }
+    }
+
+    return(
+        <div>
+            <input type={"button"} value="Click me" onClick={changeSize}/>
+            <div className={"cercle " + size}/>
+        </div>
+    )
+}
+
+export default TestState;
